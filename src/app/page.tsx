d3cf429d56229b1998704eb2e579fc134d65fbf8
Harsh G <hgaur491@gmail.com>
2025-06-15 16:22:51 +0530

    feat: add new section components and update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import About from "./_components/sections/About";
+import Contect from "./_components/sections/Contect";
+import Experience from "./_components/sections/Experience";
+import Hero from "./_components/sections/Hero";
+import Projects from "./_components/sections/Projects";
+import Skills from "./_components/sections/Skills";
+
 export default function Home() {
     return (
-        <main>
-            <h1>Hello</h1>
-        </main>
+        <div>
+            <main>
+                <Hero />
+                <About />
+                <Skills />
+                <Projects />
+                <Experience />
+                <Contect />
+            </main>
+        </div>
     );
 }
