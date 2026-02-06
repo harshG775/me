56058ad70430f7624ad615cd26f95218d04b9e0f
harshG775 <hgaur491@gmail.com>
2026-02-06 18:57:52 +0530

    //

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -18,28 +18,57 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
     loader: async () => {
         const theme = await getThemeServerFn()
-        return { theme }
+        return { host: "www.harshgaur.in", theme }
     },
-    head: () => ({
-        meta: [
-            {
-                charSet: "utf-8",
-            },
-            {
-                name: "viewport",
-                content: "width=device-width, initial-scale=1",
-            },
-            {
-                title: "TanStack Start Starter",
-            },
-        ],
-        links: [
-            {
-                rel: "stylesheet",
-                href: appCss,
-            },
-        ],
-    }),
+    head: ({ loaderData }) => {
+        const title = "Harsh Gaur — Frontend Engineer | React, Next.js, TypeScript"
+
+        const description =
+            "Portfolio of Harsh Gaur, a Frontend Engineer with 2+ years of experience building scalable, high-performance web applications using React, Next.js, TypeScript, and TanStack."
+
+        const image = `${loaderData?.host}/logo.png`
+
+        const url = `${loaderData?.host}`
+
+        return {
+            meta: [
+                {
+                    charSet: "utf-8",
+                },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+                // SEO
+                { title: title },
+                { name: "description", content: description },
+                {
+                    name: "keywords",
+                    content: "movies, tv shows, streaming, watch online, latest movies, series, OnyxStream",
+                },
+
+                // Open Graph
+                { property: "og:type", content: "website" },
+                { property: "og:title", content: title },
+                { property: "og:description", content: description },
+                { property: "og:image", content: image },
+                { property: "og:url", content: url },
+
+                // Twitter
+                { name: "twitter:card", content: "summary_large_image" },
+                { name: "twitter:title", content: title },
+                { name: "twitter:description", content: description },
+                { name: "twitter:image", content: image },
+                { name: "twitter:url", content: url },
+            ],
+            links: [
+                {
+                    rel: "stylesheet",
+                    href: appCss,
+                },
+            ],
+        }
+    },
 
     shellComponent: RootDocument,
 })
