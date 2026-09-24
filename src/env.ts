1c3b394690d48d96dfce42f9baa2ab84ee47e435
Harsh_G <hgaur491@gmail.com>
2026-09-24 11:42:26 +0530

    init tanstack-start

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+    server: {
+        SERVER_URL: z.string().url().optional(),
+    },
+    clientPrefix: "VITE_",
+    client: {
+        VITE_APP_TITLE: z.string().min(1).optional(),
+    },
+    runtimeEnv: {
+        ...process.env,
+        ...import.meta.env,
+    },
+    emptyStringAsUndefined: true,
+});
