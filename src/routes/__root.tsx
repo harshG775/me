e63a8cd3c6b4c4bd58175e1f07b52489564adff1
Harsh_G <hgaur491@gmail.com>
2026-09-25 12:16:48 +0530

    feat(theme): update styles structure

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/reac
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { getThemeInitScript, ThemeProvider } from "#/components/providers/theme-provider";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
-import appCss from "../styles.css?url";
+import appCss from "../styles/global.css?url";
 import { Footer } from "./-components/footer";
 import { Header } from "./-components/header";
 
